fbf15daf537dbf3a3b352b74b17c2e375a9e25eb
orion76 <gpb@yandex.ru>
2021-07-06 04:38:45 +0000

    fix main menu

--- src/app/content/main-menu.content.ts
+++ src/app/content/main-menu.content.ts
@@ -3,5 +3,5 @@ import {MenuItem} from 'primeng/api';
 export const mainMenuItems: MenuItem[] = [
   {label: 'Политические организации', routerLink: '/', fragment: 'section-one'},
   {label: 'Социальные сети', routerLink: '/', fragment: 'section-two'},
-  {label: 'СМИ', routerLink: '/', fragment: 'section-three'},
+  {label: 'Блогеры,СМИ', routerLink: '/', fragment: 'section-three'},
 ];
