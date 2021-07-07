ce2c58fc8059a7fbe510a6815d861a2650fd9420
orion76 <gpb@yandex.ru>
2021-07-07 03:11:41 +0000

    add meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component} from '@angular/core';
 import {MenuItem} from 'primeng/api';
 import {mainMenuItems} from './content/main-menu.content';
+import {Meta} from '@angular/platform-browser';
 
+
+const metaDescription = `Обзор интернет-ресурсов, посвященных Прямой демократии, народовластию и самоуправлению`;
+const metaKeywords = `прямая демократия, народовластие, самоуправление`;
 
 @Component({
   selector: 'app-root',
@@ -12,6 +16,8 @@ export class AppComponent {
   public items: MenuItem[] = mainMenuItems;
   title = 'ng-page';
 
-  constructor() {
+  constructor(private meta: Meta) {
+    this.meta.addTag({name: 'description', content: metaDescription});
+    this.meta.addTag({name: 'keywords', content: metaKeywords});
   }
 }
